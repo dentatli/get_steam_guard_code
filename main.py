c141ead49d395c8db97f46154f28fffc67fdc4de
dentatli <dentatlik@gmail.com>
2025-06-22 19:24:16 +0500

    Still work

--- main.py
+++ main.py
@@ -11,7 +11,10 @@ TEST_PASSWORD_MAIL = os.getenv('TEST_PASSWORD_MAIL')
 
 async def main():
     guard_code = await get_auth_code_steam(TEST_USERNAME_MAIL, TEST_PASSWORD_MAIL)
-    print(guard_code)
+    if guard_code:
+        print(f"Ваш код: {guard_code}")
+    else:
+        print("Кода нет.")
 
 async def get_auth_code_steam(login_mail: str, password_mail: str):
     url = ("https://api.firstmail.ltd/v1/market/get/message?"
@@ -20,19 +23,16 @@ async def get_auth_code_steam(login_mail: str, password_mail: str):
         "accept": "application/json",
         "X-API-KEY": API_KEY_MAIL
     }
-    async with aiohttp.ClientSession() as session:
-        response = await session.get(url, headers=headers)
-    response_json = await response.json()
-    print(response_json)
+    connector = aiohttp.TCPConnector(ssl=False)
+    async with aiohttp.ClientSession(connector=connector) as session:
+        async with session.get(url, headers=headers) as response:
+            response_json = await response.json()
     if not response_json['has_message']:
         return False
-    find_text = ('color:#3a9aed; font-weight:bold; text-align:center;">'
-                 '\/n\/t\/t\/t\/t\/t\/t\/t\/t\/t\/t\/t\/t')
-    print(find_text)
-    guard_in_text = str(response_json['message'])
+    find_text = 'color:#3a9aed;'
+    guard_in_text = response_json['message']
     index_in_text = guard_in_text.find(find_text)
-    print(index_in_text)
-    guard_code = guard_in_text[index_in_text+20:index_in_text+25]
+    guard_code = guard_in_text[index_in_text+66:index_in_text+71]
     return guard_code
 
 if __name__ == '__main__':
